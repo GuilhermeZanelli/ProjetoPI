b04293e361886be1cdda04342316ee1873f619a4
user <user@DESKTOP-51APB4C>
2025-05-07 21:03:35 -0300

    Versão da conexão do banco de dados com a porta local

---
+++ src/dao/Conexao.java
@@ -0,0 +1,14 @@
+package dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+//classe responsavel por conectar o banco de dados com o sistema
+public class Conexao {
+    private static final String URL = "jdbc:mysql://localhost:3306/poliedro_sdm";
+    private static final String USUARIO = "root";
+    private static final String SENHA = "178549";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, SENHA);
+    }
+}
