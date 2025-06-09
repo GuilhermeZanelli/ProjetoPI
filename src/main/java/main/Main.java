21628ceefb9a1915898e4aaddd0a756a92b9d39e
blueprint0 <guilhermegois2007@gmail.com>
2025-06-09 12:58:02 -0300

    test: testa a classe Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,8 +5,8 @@ import view.TelaLogin;
 public class Main {
     public static void main(String[] args) {
         
-        SwingUtilities.invokeLater(() -> {
-            TelaLogin telaLogin = new TelaLogin();
+        SwingUtilities.invokeLater(() -> {    //isso garante que a interface gráfica seja criada na thread de eventos do Swing
+            TelaLogin telaLogin = new TelaLogin();                     //(bom funcionamento do Swing)
             telaLogin.setVisible(true);
         });
     }
